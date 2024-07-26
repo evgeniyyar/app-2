99d6fef76c6cc2256eb8675b02a4a57ce8654e33
Evgeniy_Ogurtsov_laptop <eaogurtsov@gmail.com>
2024-07-26 22:29:06 +0300

    Send email using the form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,4 +1,6 @@
 import streamlit
+from send_email import send_email
+
 
 streamlit.header("Contact Me")
 
@@ -7,13 +9,5 @@ with streamlit.form(key="email_forms"):
     message = streamlit.text_area("Your message")
     button = streamlit.form_submit_button("Submit")
     if button:
-        print(button)
-
-
-
-
-
-
-
-
-
+        send_email(message)
+        streamlit.info("Your email was sent successfully")
